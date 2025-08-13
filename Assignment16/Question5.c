73d49d512978c11865a034cdf5dcfb5a7847d239
Bhagyashri-F <bhagyashrifulari21@gmail.com>
2025-08-13 12:10:59 +0530

    Add files via upload

---
+++ Assignment16/Question5.c
@@ -0,0 +1,54 @@
+//input 4   4
+/*
+1   2   3   4
+    2   3   4
+        3   4
+            4
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+    int i = 0, j = 0;
+
+    if((iRow < 0) || (iCol < 0))
+    {
+        printf("You entered invalid number of parameters : \n");
+        return ;
+    }
+
+    if(iRow != iCol)
+    {
+        printf("Please enter same number of rows and columns \n");
+        return ;
+    }
+
+    for(i = 1; i <= iCol; i++)
+    {
+        for(j = 1; j <= iRow; j++)
+        {
+            if(i <= j)
+            {
+                printf("%d  ",j);
+            }
+            else
+            {
+                printf("   ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
