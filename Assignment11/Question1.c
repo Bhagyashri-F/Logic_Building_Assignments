be454f92afdc1d9c04c634d70a1d54ace7c67611
Bhagyashri-F <bhagyashrifulari21@gmail.com>
2025-08-13 12:04:09 +0530

    Add files via upload

---
+++ Assignment11/Question1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    char ch = 'A';
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt <iNo ; iCnt++)
+    {
+        printf("%c  ",ch);
+        ch++;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number of elements : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
